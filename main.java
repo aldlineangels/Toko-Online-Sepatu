eeb1f3d019de13bfc8d1c0db3c3c0039b9fbb881
aldlineangels <104970889+aldlineangels@users.noreply.github.com>
2022-06-16 20:27:52 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,22 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Selain rajin belajar harus rajin berlatih");
+		System.out.println("sabtu dan Minggu adalah hari weekend ku");
+		System.out.println("Nasi padang makanan favorite banyak orang");
+		
+		System.out.format("Selain rajin belajar harus rajin berlatih");
+		System.out.format("sabtu dan Minggu adalah hari weekend ku");
+		System.out.format("Nasi padang makanan favorite banyak orang");
+		
+		System.out.print("Selain rajin belajar harus rajin berlatih");
+		System.out.print("sabtu dan Minggu adalah hari weekend ku");
+		System.out.print("Nasi padang makanan favorite banyak orang");
+		
+		
+		
+	}
+
+}
